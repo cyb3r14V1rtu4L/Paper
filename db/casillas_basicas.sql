fa3964d412b4793bd4a53422be4b2fff7c2af97e
Cyberio L.0 <cybberio@gmail.com>
2019-04-06 03:29:03 -0600

    Mpi0S

--- db/casillas_basicas.sql
+++
@@ -1,7 +0,0 @@
-UPDATE users
-  SET username = Replace(username, 'BASICA', 'B1')
-WHERE username LIKE '%BASICA%';
-
-UPDATE xmf_casillas
-  SET name = Replace(name, 'BASICA', 'B1')
-WHERE name LIKE '%BASICA%';
